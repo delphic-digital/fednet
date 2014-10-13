0b16579430c96ee32e78e64feb23e64d763571e1
Jeff Mills <jeffy9@gmail.com>
2014-10-12 22:12:31 -0400

    shit load of changes

---
+++ build.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var path        = require('path');
+var Nunjucks    = require('nunjucks');
+var metalsmith  = require('metalsmith');
+var markDown    = require('metalsmith-markdown');
+var sass        = require('metalsmith-ruby-sass');
+var templates   = require('metalsmith-templates');
+var navigation  = require('metalsmith-navigation');
+var watch       = require('metalsmith-watch');
+
+
+var navTask = navigation(navConfigs);
+
+var navConfigs = {
+    primary:{
+        sortBy: 'nav_sort',
+        filterProperty: 'nav_groups',
+    }
+};
+
+var relativePathHelper = function(current, target) {
+    // normalize and remove starting slash from path
+    if(!current || !target){
+        return '';
+    }
+    current = path.normalize(current).slice(0);
+    target = path.normalize(target).slice(0);
+    current = path.dirname(current);
+    return path.relative(current, target);
+};
+
+var navTask = navigation(navConfigs);
+
+var markDownTask = markDown();
+var watchTask = watch({
+    pattern : ['**/*', '../templates/**/*'],
+    livereload: true
+  });
+
+var sassTask = sass({
+    outputDir: 'css/',
+    outputStyle: 'compressed'
+})
+
+var templatesTask = templates({
+    engine: 'nunjucks',
+    settings:  {
+        views: 'templates',
+        filters: {
+            'relativePath': relativePathHelper
+        }
+    }
+});
+
+var meta = {
+    siteTitle: 'Underware',
+    description: 'Front end developer knowledge base.',
+};
+
+
+
+var metalsmith = metalsmith(__dirname)
+    .clean(true)
+    .metadata(meta)
+    .use(markDownTask)
+    .use(sassTask)
+    .use(navTask)
+    .use(templatesTask)
+   //.use(watchTask)
+    .build(function(err) {
+        if (err) throw err;
+    });
---
+++ src/tutorials/tutorial-two.md
@@ -0,0 +1,9 @@
+---
+title: Tutorial 2
+template: page.html
+nav_sort: 4
+nav_groups:
+  - primary
+---
+
+Here is the second tutorial page
