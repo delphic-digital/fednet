46d85ff5f005fb146ca8af6ff4bed86e9077acf9
Jeff <jmills@delphicdigital.com>
2016-06-06 17:04:56 -0400

    Use github api to get commit info of page

--- themes/fednet/public/scripts/raneto.js
+++ themes/fednet/public/scripts/raneto.js
@@ -30,6 +30,31 @@
       });
     }
 
+    //delphic-digital:cafc7d30d1415b8574f49befabcd3c07cabc252f
+
+     var dd = new Gh3.User("delphic-digital")
+     var fednet = new Gh3.Repository("fednet", dd);
+     fednet.fetchBranches(function (err, res) {
+       var v2 = fednet.getBranchByName("v2");
+        v2.fetchContents(function (err, res) {
+         var contents = v2.getDirByName('content');
+
+          contents.fetchContents(function (err, res) {
+            var slug = $('.page.active a').attr('href');
+            //console.log(contents.getContents())
+            var page = contents.getFileByName(slug.substring(1)+".md");
+             page.fetchCommits(function (err, res) {
+              var lastCommit = page.getLastCommit();
+              var time = moment(lastCommit.author.date).format("ddd, MMM Do, h:mm:ss a");
+
+              $('.last-updated').html('Last updated on '+time+' by '+lastCommit.author.name+ ' '+'<a href="https://github.com/delphic-digital/fednet/commit/'+lastCommit.sha+'">'+lastCommit.sha+'</a>')
+             })
+          })
+        })
+
+     })
+
+
   });
 
 })(jQuery, hljs);
